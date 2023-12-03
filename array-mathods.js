81f394ebfad5fe38074dfb8cd3e962a800500059
Akram <sbntech72@gmail.com>
2023-12-03 02:40:53 -0800

    add new Practice file

---
+++ array-mathods.js
@@ -0,0 +1,113 @@
+
+//  1. Array To String       toString()
+//  2. string to array        split()
+
+
+
+
+
+
+
+
+let Akram = ['Akram' , 'Hassan' , 'Vikky' , 'Himanshu' , 'Saqib']
+
+
+
+
+//Array To String 
+
+// // let ArrayToString = Akram.toString()
+// console.log(ArrayToString)
+// document.write(ArrayToString)
+
+
+// to array
+
+// let mobile = '7292937945'
+
+// let mobile_array = mobile.split("")
+// console.log(mobile_array[0],mobile_array[1])
+// document.write(mobile_array)
+
+
+
+
+
+// pop and push Function
+
+
+// // push function
+// Add Element From The Last 
+
+
+// Akram.push('Ibraheem')
+// console.log(Akram)
+// document.write(Akram)
+
+
+// pop Function
+// remove Element From The Last 
+
+//  Akram.pop()
+//  document.write(Akram)
+//  console.log(Akram)
+
+
+
+
+
+
+// Shift and Unshift
+
+// Shift Function
+// Remove Element From The Start 
+
+// Akram.shift()
+
+// document.write(Akram)
+// console.log(Akram)
+
+
+
+
+// // Unshift Function
+// // Add Element From The Start
+// Akram.unshift('Ibraheem')
+// document.write(Akram)
+// console.log(Akram)
+
+
+
+
+// Splice To Add Element at Center
+
+// Akram.splice(1,0,'Sonu','Danish')
+// Akram.splice(0,1,'Sonu')
+// document.write(Akram)
+
+// ============================================================
+
+
+
+   // let arr1 = ['1', '2', '3' , '4' , '5']
+
+   // let arr2 = ['6', '7' ,'8', '9' , '10']
+
+   // let arr3 = arr1.join(arr2)
+   
+   // document.write(arr3)
+   
+   // console.log(arr3)
+
+
+
+
+
+
+   // delete function for delete a Element From Array
+
+   // delete Akram[2]
+
+   // console.log(Akram)
+
+   

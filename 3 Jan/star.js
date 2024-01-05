57eae60ff339215eceb443e5f39102a948121d0b
AkramCodes <sbntech72@gmail.com>
2024-01-05 13:02:46 +0530

    add json file

--- 3 Jan/star.js
+++ 3 Jan/star.js
@@ -1,68 +1,139 @@
 
 
 
-const jSon = JSON.parse(`{
-    "id": 1,
-    "firstName": "Terry",
-    "lastName": "Medhurst",
-    "maidenName": "Smitham",
-    "age": 50,
-    "gender": "male",
-    "email": "atuny0@sohu.com",
-    "phone": "+63 791 675 8914",
-    "username": "atuny0",
-    "password": "9uQFF1Lh",
-    "birthDate": "2000-12-25",
-    "image": "https://robohash.org/hicveldicta.png",
-    "bloodGroup": "A−",
-    "height": 189,
-    "weight": 75.4,
-    "eyeColor": "Green",
-    "hair": {
-        "color": "Black",
-        "type": "Strands"
-    },
-    "domain": "slashdot.org",
-    "ip": "117.29.86.254",
-    "address": {
-        "address": "1745 T Street Southeast",
-        "city": "Washington",
-        "coordinates": {
-            "lat": 38.867033,
-            "lng": -76.979235
-        },
-        "postalCode": "20020",
-        "state": "DC"
-    },
-    "macAddress": "13:69:BA:56:A3:74",
-    "university": "Capitol University",
-    "bank": {
-        "cardExpire": "06/22",
-        "cardNumber": "50380955204220685",
-        "cardType": "maestro",
-        "currency": "Peso",
-        "iban": "NO17 0695 2754 967"
-    },
-    "company": {
-        "address": {
-            "address": "629 Debbie Drive",
-            "city": "Nashville",
-            "coordinates": {
-                "lat": 36.208114,
-                "lng": -86.58621199999999
-            },
-            "postalCode": "37076",
-            "state": "TN"
-        },
-        "department": "Marketing",
-        "name": "Blanda-O'Keefe",
-        "title": "Help Desk Operator"
-    },
-    "ein": "20-9487066",
-    "ssn": "661-64-2976",
-    "userAgent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24"
-}`)
+// const jSon = JSON.parse(`{
+//     "id": 1,
+//     "firstName": "Terry",
+//     "lastName": "Medhurst",
+//     "maidenName": "Smitham",
+//     "age": 50,
+//     "gender": "male",
+//     "email": "atuny0@sohu.com",
+//     "phone": "+63 791 675 8914",
+//     "username": "atuny0",
+//     "password": "9uQFF1Lh",
+//     "birthDate": "2000-12-25",
+//     "image": "https://robohash.org/hicveldicta.png",
+//     "bloodGroup": "A−",
+//     "height": 189,
+//     "weight": 75.4,
+//     "eyeColor": "Green",
+//     "hair": {
+//         "color": "Black",
+//         "type": "Strands"
+//     },
+//     "domain": "slashdot.org",
+//     "ip": "117.29.86.254",
+//     "address": {
+//         "address": "1745 T Street Southeast",
+//         "city": "Washington",
+//         "coordinates": {
+//             "lat": 38.867033,
+//             "lng": -76.979235
+//         },
+//         "postalCode": "20020",
+//         "state": "DC"
+//     },
+//     "macAddress": "13:69:BA:56:A3:74",
+//     "university": "Capitol University",
+//     "bank": {
+//         "cardExpire": "06/22",
+//         "cardNumber": "50380955204220685",
+//         "cardType": "maestro",
+//         "currency": "Peso",
+//         "iban": "NO17 0695 2754 967"
+//     },
+//     "company": {
+//         "address": {
+//             "address": "629 Debbie Drive",
+//             "city": "Nashville",
+//             "coordinates": {
+//                 "lat": 36.208114,
+//                 "lng": -86.58621199999999
+//             },
+//             "postalCode": "37076",
+//             "state": "TN"
+//         },
+//         "department": "Marketing",
+//         "name": "Blanda-O'Keefe",
+//         "title": "Help Desk Operator"
+//     },
+//     "ein": "20-9487066",
+//     "ssn": "661-64-2976",
+//     "userAgent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24"
+// }`)
 
 
-console.log(jSon)
-document.write(jSon)
+// console.log(jSon)
+
+
+
+
+// let obj = {
+
+//     building: {
+//         dimension: ["500Sqft * 400sqft"],
+//         pantColor: ["white", "red", "Green", "Yellow"],
+//         practiceRoom: 3,
+//         classRoom: 3,
+//     },
+//     teacher: {
+//         forWeb: [
+//             "Mr. Rohit Kashyap", "Mr. Suraj "
+//         ],
+//         forGraphicDesigner: [
+//             "Mr. Zakir Khan", "Mr. Shree Bharat"
+//         ],
+//         forVideoAndMotionGraphics: ["Mr. Zakir Khan", "Mr. Shree Bharat"],
+//         forItSolution: "Mr. Intezar Ali ",
+//         forInterior: "Mr. Akash",
+//         forPlacement: "Mr. Agraj Sir",
+//         forPersonalityDevelopment: "Ms Jyoti Yadav",
+//         forAccount: "Mr.  Moaz Sir",
+
+//     },
+//     forhousekeeping: "Mr. Shyam and Mr.Jay",
+
+//     forhr: "Ms Yasmeen Khan",
+//     forAddmin: "Ms Muskan Bakhshi",
+//     ownerOfTgc: "Mr. Ravi Sir",
+
+//     allBatchOfWeb: {
+//         firstBatch: {
+//             time: "10:00 AM",
+//             faculty: "Mr Rohit Kashyap",
+//             studentsName: [
+//                 "Akram", "Niklesh", "Himanshu", "Hassan", "Saqib", "Rohit", "Surjeet"
+//             ],
+
+//             SecondBatch: {
+//                 time: "1:00 PM",
+//                 faculty: "Mr Rohit Kashyap",
+//                 studentsName: [
+//                     "Akram", "Niklesh", "Himanshu", "Hassan", "Saqib", "Rohit", "Surjeet"
+//                 ]
+
+//             }
+//         }
+//     }
+// }
+
+// // console.log(obj)
+
+// let convart = JSON.stringify(obj)
+// console.log(convart)
+
+
+
+
+
+
+
+
+let taxt = `{ "building": { "dimension": ["500Sqft * 400sqft"], "pantColor": ["white", "red", "Green", "Yellow"], "practiceRoom": 3, "classRoom": 3 }, "teacher": { "forWeb": ["Mr. Rohit Kashyap", "Mr. Suraj "], "forGraphicDesigner": ["Mr. Zakir Khan", "Mr. Shree Bharat"], "forVideoAndMotionGraphics": ["Mr. Zakir Khan", "Mr. Shree Bharat"], "forItSolution": "Mr. Intezar Ali ", "forInterior": "Mr. Akash", "forPlacement": "Mr. Agraj Sir", "forPersonalityDevelopment": "Ms Jyoti Yadav", "forAccount": "Mr.  Moaz Sir" }, "forhousekeeping": "Mr. Shyam and Mr.Jay", "forhr": "Ms Yasmeen Khan", "forAddmin": "Ms Muskan Bakhshi", "ownerOfTgc": "Mr. Ravi Sir", "allBatchOfWeb": { "firstBatch": { "time": "10:00 AM", "faculty": "Mr Rohit Kashyap", "studentsName": ["Akram", "Niklesh", "Himanshu", "Hassan", "Saqib", "Rohit", "Surjeet"], "SecondBatch": { "time": "1:00 PM", "faculty": "Mr Rohit Kashyap", "studentsName": ["Akram", "Niklesh", "Himanshu", "Hassan", "Saqib", "Rohit", "Surjeet"] } } } }`
+
+
+// let objectx = JSON.parse(taxt)
+
+let result =  JSON.parse(taxt)
+console.log(result )
